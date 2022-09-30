d36e7d6d4e75e46158fc05ac103e2c925ecd6871
EC2 Default User <ec2-user@ip-172-31-11-175.ap-northeast-1.compute.internal>
2022-09-30 07:11:18 +0000

    Initial Commit

---
+++ app/models/customer.rb
@@ -0,0 +1,40 @@
+class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  has_many :addresses, dependent: :destroy
+  
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+ 
+  validates :email, uniqueness: true
+
+  def self.search_for(content,method)
+    return none if content.blank?
+     if method == 'forward'
+      # カラムが2つの部分を1つに補う
+       f_customer = Customer.where('first_name LIKE ?',content + '%')
+       l_customer = Customer.where('last_name LIKE ?',content + '%')
+       customers = f_customer + l_customer
+     elsif method == 'backword'
+       Customer.where('first_name LIKE ?', '%' + content)
+       f_customer = Customer.where('first_name LIKE ?', '%' + content)
+       l_customer = Customer.where('last_name LIKE ?', '%' + content)
+       
+       customers = f_customer + l_customer
+     else
+       f_customer = Customer.where('first_name LIKE ?', '%' + content + '%')
+       l_customer = Customer.where('last_name LIKE ?', '%' + content + '%')
+       
+       customers = f_customer + l_customer
+     end
+  end
+  
+  
+         
+         
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,40 @@
 Rails.application.routes.draw do
+
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 顧客用
+  get 'about' => 'public/homes#about'
+  root 'public/homes#top'
+  get '/customers/contact' => 'public/customers#contact'
+
+  scope module: :public do
+    resources :items,only: [:index,:show]
+    get 'search' => 'items#search'
+    # deviseと衝突してしまうので、オリジナルに変更
+    get 'edit/customers' => 'customers#edit'
+    patch 'update/customers' => 'customers#update'
+    
+  	resource :customers,only: [:edit,:update,:show] do
+  		collection do
+  	     get 'quit'
+  	     patch 'out'
+  	  end
+  	 end
+  	end
+  	  
+
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
