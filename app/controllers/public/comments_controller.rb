97b28896ff924bd5d65c85589401c1670dce4a53
EC2 Default User <yasuzyun@yahoo.co.jp>
2022-12-16 13:49:39 +0000

    Initial Commit

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -14,7 +14,8 @@ class Public::CommentsController < ApplicationController
   end
 
 def destroy
-    @comment = Comment.find(params[:id])
+    #@article = Article.find(params[:id])
+    @comment = current_customer.comments.find_by(article_id: @article.id)
   if @comment.destroy
     redirect_to article_path(@article), notice: 'コメントを削除しました'
   else
