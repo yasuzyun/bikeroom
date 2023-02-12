12758523b6bd062550ae3a7a539b75ea3052604f
EC2 Default User <yasuzyun@yahoo.co.jp>
2023-02-12 04:25:11 +0000

    Initial Commit

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,13 +2,12 @@ class Public::SearchesController < ApplicationController
   before_action :authenticate_customer!
 
 	def search
-		@model = params[:model]
-		@content = params[:content]
-		@method = params[:method]
-		if @model == 'customer'
-			@records = Customer.search_for(@content, @method)
-		else
-			@records = Article.search_for(@content, @method)
-		end
+		@range = params[:range]
+
+    if @range == "Customer"
+      @customers = Customer.looks(params[:search], params[:word])
+    else
+      @articles = Article.looks(params[:search], params[:word])
+    end
 	end
 end
